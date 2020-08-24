7699301e022d764b8f566a1d63fb920202f0ea42
Spencer_Vail <vail.spencer@gmail.com>
2020-08-24 12:43:58 -0700

    initial file structure

---
+++ src/test/java/com/lambdaschool/howto/HowtoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.howto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowtoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
